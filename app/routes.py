b3e0fdd55d1ff65e10e171ce420a147d47d18c68
noi <dhshsvd700@gmail.com>
2025-05-17 15:54:09 +0800

    4

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,5 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-#1
 @app.route('/hello')
 def hello():
     return f'Hello, world!'
@@ -11,14 +8,10 @@ def hello():
 def info():
     return f'This is an informational page'
 
-
-#2
 @app.route('/calc/<int:numb_1>/<int:numb_2>')
 def calc(numb_1, numb_2):
     return f'The sum of {numb_1} and {numb_2} is {numb_1 + numb_2}'
 
-
-#3
 @app.route('/reverse/<text>')
 def reverse(text):
     if len(text) < 1 or text == ' ':
@@ -26,15 +19,9 @@ def reverse(text):
     else:
         return text[::-1]
 
-
-#4
 @app.route('/user/<name>/<int:age>')
 def user(name, age):
     if age <= 0:
         return 'You are too small'
     else:
         return f'Hello, {name}. You are {age} years old'
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
