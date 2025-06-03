b925fa2c10c5ccb6e47680ddd195c9a74cbdb9d0
noi <dhshsvd700@gmail.com>
2025-06-03 19:32:08 +0800

    Практикум 1 Home, About, Contact

--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,34 @@
 from flask import render_template, request, redirect, url_for
 from app import app
-import random
 
-list_joke = ["God gave man teeth so that he could live, and toothache so that he would clearly know his place..",
-             "Everything happens the first time, but not everything happens the second time..",
-             "Why do birds fly south in winter? - Because flying is faster than walking..",
-             "If you are imprisoned for tax evasion, you will be held for taxes that you have not already paid..",
-             "I had a fit of vivacity this morning. But he attacked the wrong one…"]
-
+valid_mail = ('@mail.ru', '@mail.com', '@bk.ru', '@yandex.ru', '@rambler.ru',
+              '@live.ru', '@hotmail.com', '@gmail.com', '@icq.com', '@qip.ru')
 
 @app.route('/')
 def form():
-    return render_template('form.html')
+    return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/submit', methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
         name = request.form.get("name")
-        color = request.form.get("color")
-        profession = request.form.get("profession")
-        hobbies = request.form.getlist("hobbies")
-        level = request.form.get("level")
+        email = request.form.get("email")
+        message = request.form.get("message")
 
-        if not name:
-            error_message = "We can't process your form without your name, Mr. Anonymous!"
-            return error_message
-
-        elif not color:
-            error_message = "We are very interested in what your favorite color is, please indicate it!"
-            return error_message
-
-        elif not profession:
-            error_message = "Are you unemployed?"
-            return error_message
-
-        elif not hobbies:
-            error_message = "What about your hobby?"
-            return error_message
-
-        elif not level:
-            error_message = "You didn't specify your level."
-            return error_message
-
-        joke = random.choice(list_joke)
-        return render_template("result.html", name=name, color=color,
-                               profession=profession, hobbies=hobbies, level=level, joke=joke)
+        if email.endswith(valid_mail) is False:
+            return "Invalid email format"
+        else:
+            return render_template("result.html", name=name, email=email, message=message)
 
     else:
-        return redirect(url_for("form"))
+        return redirect(url_for("index"))
 
 
